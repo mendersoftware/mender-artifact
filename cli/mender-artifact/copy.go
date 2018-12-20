580d7aa0b8090c6529b68d81720a6e166ebc99be
Ole Petter <ole.orhagen@cfengine.com>
2018-12-20 17:22:21 +0100

    Revert "Merge pull request #160 from oleorhagen/fixcat"
    This reverts commit 7438c80d73c6470395d394eae714a376ff7a50a3, reversing
    changes made to 5d186d48133181539162949423298e5a3ced1147.

--- cli/mender-artifact/copy.go
+++ cli/mender-artifact/copy.go
@@ -41,12 +41,11 @@ func Cat(c *cli.Context) (err error) {
 	if err != nil {
 		return cli.NewExitError(fmt.Sprintf("failed to open the partition reader: err: %v", err), 1)
 	}
-	defer r.Close()
 	var w io.WriteCloser = os.Stdout
 	if _, err = io.Copy(w, r); err != nil {
 		return cli.NewExitError(fmt.Sprintf("failed to copy from: %s to stdout: err: %v", c.Args().First(), err), 1)
 	}
-	return nil
+	return r.Close()
 }
 
 func Copy(c *cli.Context) (err error) {
