73c81804166047acdc094745398ef923627cb520
Kristian Amlie <kristian.amlie@northern.tech>
2019-01-14 16:37:14 +0100

    Expand UpdateStorerProducer interface to include type in constructor.
    This is needed because one UpdateStorerProducer may support multiple
    payload types (infinitely many in the case of update modules), so we
    need to pass the type to differentiate between them.
    
    Changelog: None
    
    Signed-off-by: Kristian Amlie <kristian.amlie@northern.tech>

--- handlers/common.go
+++ handlers/common.go
@@ -97,7 +97,7 @@ type UpdateStorer interface {
 }
 
 type UpdateStorerProducer interface {
-	NewUpdateStorer(payloadNum int) (UpdateStorer, error)
+	NewUpdateStorer(updateType string, payloadNum int) (UpdateStorer, error)
 }
 
 type Installer interface {
@@ -120,9 +120,9 @@ func (i *installerBase) SetUpdateStorerProducer(producer UpdateStorerProducer) {
 	i.updateStorerProducer = producer
 }
 
-func (i *installerBase) NewUpdateStorer(payloadNum int) (UpdateStorer, error) {
+func (i *installerBase) NewUpdateStorer(updateType string, payloadNum int) (UpdateStorer, error) {
 	if i.updateStorerProducer != nil {
-		return i.updateStorerProducer.NewUpdateStorer(payloadNum)
+		return i.updateStorerProducer.NewUpdateStorer(updateType, payloadNum)
 	} else {
 		return &devNullUpdateStorer{}, nil
 	}
