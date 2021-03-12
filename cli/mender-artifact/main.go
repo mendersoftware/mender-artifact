86d92354ea03cb1ee88a1f156c113d06ba69e080
Ole Petter <ole.orhagen@northern.tech>
2021-03-12 14:56:03 +0100

    Remove the '--compression' flag as global in the helptext
    The flag was never really global anyways.
    
    This commit removes it from the global option, and adds a note notifying the
    user that the flag functions as a global flag, but only for certain commands.
    
    Changelog: Add a note about the proper usage of the 'compression' flag in the
    global help text.
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>
    (cherry picked from commit 0c052e740b4766026649f9be716a53868b888df2)

--- cli/mender-artifact/main.go
+++ cli/mender-artifact/main.go
@@ -49,6 +49,38 @@ func main() {
 	}
 }
 
+// Copied from urfave/cli/template.go
+// with the addition of the NOTE on the `global` `--compression flag`
+var menderAppHelpTemplate = `NAME:
+   {{.Name}}{{if .Usage}} - {{.Usage}}{{end}}
+
+USAGE:
+   {{if .UsageText}}{{.UsageText}}{{else}}{{.HelpName}} {{if .VisibleFlags}}[global options]{{end}}{{if .Commands}} command [command options]{{end}} {{if .ArgsUsage}}{{.ArgsUsage}}{{else}}[arguments...]{{end}}{{end}}{{if .Version}}{{if not .HideVersion}}
+
+VERSION:
+   {{.Version}}{{end}}{{end}}{{if .Description}}
+
+DESCRIPTION:
+   {{.Description}}{{end}}{{if len .Authors}}
+
+AUTHOR{{with $length := len .Authors}}{{if ne 1 $length}}S{{end}}{{end}}:
+   {{range $index, $author := .Authors}}{{if $index}}
+   {{end}}{{$author}}{{end}}{{end}}{{if .VisibleCommands}}
+
+COMMANDS:{{range .VisibleCategories}}{{if .Name}}
+
+   {{.Name}}:{{range .VisibleCommands}}
+     {{join .Names ", "}}{{"\t"}}{{.Usage}}{{end}}{{else}}{{range .VisibleCommands}}
+   {{join .Names ", "}}{{"\t"}}{{.Usage}}{{end}}{{end}}{{end}}{{end}}{{if .VisibleFlags}}
+
+GLOBAL OPTIONS:
+   {{range $index, $option := .VisibleFlags}}{{if $index}}
+   {{end}}{{$option}}{{end}}{{end}}
+   NOTE:
+       For the commands <write>, <modify>, the '--compression' flag functions as
+       a global option
+`
+
 func applyCompressionInCommand(c *cli.Context) error {
 	// Let --compression argument work after command as well. Latest one
 	// applies.
@@ -80,6 +112,7 @@ func getCliContext() *cli.App {
 	globalCompressionFlag := compressionFlag
 	// The global flag is the last fallback, so here we provide a default.
 	globalCompressionFlag.Value = "gzip"
+	globalCompressionFlag.Hidden = true
 
 	privateKeyFlag := cli.StringFlag{
 		Name: "key, k",
@@ -507,6 +540,7 @@ func getCliContext() *cli.App {
 	sort.Sort(cli.FlagsByName(app.Flags))
 	sort.Sort(cli.CommandsByName(app.Commands))
 
+	app.CustomAppHelpTemplate = menderAppHelpTemplate
 
 	return app
 }
