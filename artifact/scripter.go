dc7d560489dcd674d328aba433a7e9b707f54b76
Ole Petter <ole.orhagen@northern.tech>
2021-04-12 11:26:39 +0200

    Log: Notify that it is the script name which is wrong
    It is easy to think that there is something inherently wrong with the script.
    
    Make sure that the user understands it is the naming which is wrong, and add a
    description and an example of the naming scheme expected.
    
    Changelog: In case of a user trying to add a script with an invalid name, the
    error message now says just so: Invalid script name, instead of simply: Invalid script.
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- artifact/scripter.go
+++ artifact/scripter.go
@@ -1,4 +1,4 @@
-// Copyright 2020 Northern.tech AS
+// Copyright 2021 Northern.tech AS
 //
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
@@ -54,7 +54,7 @@ func (s *Scripts) Add(path string) error {
 	// the second one shold be the name of the state
 	matches := re.FindStringSubmatch(name)
 	if matches == nil || len(matches) < 3 {
-		return errors.Errorf("scripter: invalid script: %s", name)
+		return errors.Errorf("scripter: invalid script name: %q. Scripts must have a name on the form: <STATE_NAME>_<ACTION>_<ORDERING_NUMBER>_<OPTIONAL_DESCRIPTION>. For example: 'Download_Enter_05_wifi-driver' is a valid script name.", name)
 	}
 	if _, found := availableScriptType[matches[1]]; !found {
 		return errors.Errorf("scripter: unsupported script state: %s", matches[1])
