mendersoftware/mender-artifact

commit ba669f039568543c19797576383749d4b55a3353

ci: Add -coverpkg when generating coverage
Adds coverage accross package boundaries.

Signed-off-by: Alf-Rune Siqveland <alf.rune@northern.tech>

Makefile
@@ -2,8 +2,7 @@ GO ?= go
 GOFMT ?= gofmt
 V ?=
 PREFIX ?= /usr/local
-PKGS = $(shell go list ./... | grep -v vendor)
-SUBPKGS = $(shell go list ./... | sed '1d' | tr '\n' ',' | sed 's/,$$//1')
+PKGS = $(shell go list ./... | tr '\n' ',' | head -c -1)
 PKGNAME = mender-artifact
 PKGFILES = $(shell find . \( -path ./vendor -o -path ./Godeps \) -prune \
 		-o -type f -name '*.go' -print)
@@ -146,7 +145,7 @@ instrument-binary:
 
 coverage:
 	rm -f coverage.txt
-	go test -tags '$(TAGS)' -covermode=atomic -coverprofile=coverage.txt ./...
+	go test -tags '$(TAGS)' -covermode=atomic -coverpkg=$(PKGS) -coverprofile=coverage.txt ./...
 
 .PHONY: build clean get-tools test check \
 	cover htmlcover coverage tooldep install-autocomplete-scripts \

commit 41c96f0542995e5621951c81e4725a7a0b856723

build: Fix `make test` target
The well-intended commit ba669f03 broke `make test` when improving
the `make coverage` target :)

Signed-off-by: Lluis Campos <lluis.campos@northern.tech>

Makefile
@@ -132,7 +132,7 @@ tooldep:
 	mtools --version
 
 test: tooldep
-	$(GO) test -v $(PKGS)
+	$(GO) test -v ./...
 
 extracheck:
 	echo "-- checking if code is gofmt'ed"